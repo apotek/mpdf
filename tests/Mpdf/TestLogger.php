351c68104325f47c58a155ea0a963c125e062e7d
apotek <279278+apotek@users.noreply.github.com>
2023-04-19 15:00:51 -0400

    Fix php code sniffer spaces to tabs

--- tests/Mpdf/TestLogger.php
+++ tests/Mpdf/TestLogger.php
@@ -6,8 +6,8 @@
 $reflection = new \ReflectionClass('\Psr\Log\AbstractLogger');
 $return_type = $reflection->getMethod('log')->getReturnType();
 if (empty($return_type)) {
-  include __DIR__ . "/psr-log-2/TestLogger.php";
+	include __DIR__ . "/psr-log-2/TestLogger.php";
 }
 else {
-  include __DIR__ . "/psr-log-3/TestLogger.php";
+	include __DIR__ . "/psr-log-3/TestLogger.php";
 }
