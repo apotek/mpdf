3d8512f8f5e513d4fc67510176c822cceed7e995
apotek <279278+apotek@users.noreply.github.com>
2023-04-20 12:53:51 -0400

    Don't assume ReflectionMethod::getReturnType() exists
    Re-support php 5.6 by not assuming the getReturnType() method
    exists on the ReflectionMethod class.

--- tests/Mpdf/TestLogger.php
+++ tests/Mpdf/TestLogger.php
@@ -3,11 +3,13 @@
 // the other with no return type declared) we adapt which class is actually
 // included based on the signature of \Psr\Log\AbstractLogger included by
 // Composer.
-$reflection = new \ReflectionClass('\Psr\Log\AbstractLogger');
-$return_type = $reflection->getMethod('log')->getReturnType();
-if (empty($return_type)) {
-	include __DIR__ . "/psr-log-2/TestLogger.php";
-}
-else {
+$loggerReflect = new \ReflectionClass('\Psr\Log\AbstractLogger');
+// Will throw desirable exception if AbstractLogger does not implement log().
+$loggerLogMethodReflect = $loggerReflect->getMethod('log');
+if (method_exists($loggerLogMethodReflect, 'getReturnType') && $loggerLogMethodReflect->getReturnType()) {
 	include __DIR__ . "/psr-log-3/TestLogger.php";
 }
+else {
+	include __DIR__ . "/psr-log-2/TestLogger.php";
+}
+
